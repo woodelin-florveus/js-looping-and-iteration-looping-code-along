7a3f19d376a40bffc065404c467a609944097155
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-31 00:36:11 -0500

    commit tree 88abfe7a0a06d5c7ce3b2c8d4990f130a26c8e02

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+function writeCards(name, event) {
+        let thanks = []
+    for(let i=0; i < name.length; i++) {
+        
+        thanks.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanks;
+}
+
+
+// function countDown(num){
+//     for (var i = num.length -1; i >= 0; i--) {
+//         console.log(num[i]) 
+//     }
+
+// }
+
+function countDown(num){
+    while (num > 0) {
+        console.log(num); 
+        num -= 1;
+    }
+    console.log(num);
+}
